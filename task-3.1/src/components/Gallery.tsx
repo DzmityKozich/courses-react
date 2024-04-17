320cfeac85d2dd08ee8fdf7596506580d76b0c53
foxstudent106668 <dmitry.kozich@gmail.com>
2024-04-17 23:10:56 +0400

    Update json data; remove zooming functionality

--- task-3.1/src/components/Gallery.tsx
+++ task-3.1/src/components/Gallery.tsx
@@ -5,8 +5,8 @@ export function Gallery({ imgs }: { imgs: Img[] }) {
 	return (
 		<>
 			<div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5">
-				{imgs.map((img) => (
-					<ImgCard img={img}></ImgCard>
+				{imgs.map((img, i) => (
+					<ImgCard img={img} key={i}></ImgCard>
 				))}
 			</div>
 		</>
--- task-3.1/src/components/ImgCard/ImgCard.tsx
+++ task-3.1/src/components/ImgCard/ImgCard.tsx
@@ -1,4 +1,3 @@
-import { useState } from 'react';
 import { Img } from '../../models/Img';
 
 import './ImgCard.scss';
@@ -8,24 +7,9 @@ interface Props {
 }
 
 export default function ImgCard({ img }: Props) {
-	const [size, setSize] = useState<{ width: number; height: number }>();
-
-	const zoomIn = () => {
-		setSize({ width: img.webformatWidth, height: img.webformatHeight });
-	};
-
-	const zoomOut = () => {
-		setSize({ width: 180, height: 135 });
-	};
-
 	return (
 		<>
-			<div
-				className="img-card bg-cover bg-center cursor-pointer"
-				// onMouseEnter={zoomIn}
-				// onMouseLeave={zoomOut}
-				style={{ backgroundImage: `url(${img.webformatURL})`, ...size }}
-			>
+			<div className="img-card bg-cover bg-center cursor-pointer" style={{ backgroundImage: `url(${img.webformatURL})` }}>
 				<p className="absolute left-0 bottom-0 w-full text-center bg-black bg-opacity-60 text-white">{img.user}</p>
 			</div>
 		</>
