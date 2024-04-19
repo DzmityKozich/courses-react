b4afdd19451a7fcc5d4fef8814414e6d6e3fb7c7
foxstudent106668 <dmitry.kozich@gmail.com>
2024-04-20 01:44:15 +0400

    Initial version

---
+++ task-3.2/src/Timer/Timer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Timer.scss';
+
+// Create custom useTimer hook
+// that will implement toggleTimer(), startTimer(), resetTimer() methods
+// and contain seconts state
+// also use useRef hook to store timout key
+
+export const Timer: React.FC<any> = () => {
+	return <></>;
+};
