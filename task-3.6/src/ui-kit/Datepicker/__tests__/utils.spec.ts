262bcc35131811e7e849206f07ace6a982f50bd9
foxstudent106668 <dmitry.kozich@gmail.com>
2024-08-09 01:52:51 +0400

    Update unit tests for Datepicker utils

--- task-3.6/src/ui-kit/Datepicker/__tests__/utils.spec.ts
+++ task-3.6/src/ui-kit/Datepicker/__tests__/utils.spec.ts
@@ -1,5 +1,13 @@
-import { vi } from 'vitest';
-import { getMonthData, getYearList, isFirstAvailableMonth, isLastAvailableMonth } from '../utils';
+import {
+	addMissingDates,
+	getDaysInMonth,
+	getMonthData,
+	getYearList,
+	isFirstAvailableMonth,
+	isLastAvailableMonth,
+	splitByWeeks,
+} from '../utils';
+import { DatepickerDate } from '../DatepikerDate';
 
 // vi.spyOn(global, 'Date');
 
@@ -26,9 +34,70 @@ describe('Datepicker utils test', () => {
 		expect(isFirstAvailableMonth(2021, 2021, 11)).toBe(false);
 	});
 
-	it('should', () => {
-		const dates = getMonthData(2021, 0);
+	it('should return days of month', () => {
+		const dates = getDaysInMonth(2021, 1);
 
-		expect(dates).toBeTruthy();
+		expect(dates).toHaveLength(28);
+		for (let index = 0; index < dates.length; index++) {
+			expect(dates[index].getDate()).toBe(index + 1);
+		}
+	});
+
+	it('should fill full datepicker page', () => {
+		const currentDate = new Date();
+		const dates = getDaysInMonth(currentDate.getFullYear(), currentDate.getMonth());
+
+		const page = addMissingDates(dates);
+
+		expect(page.length % 7).toBe(0);
+		for (let week = 0; week < page.length / 7; week++) {
+			for (let day = 0; day < 7; day++) {
+				const date = page[week * 7 + day];
+				if (day + 1 < 7) {
+					expect(date.getDay()).toBe(day + 1);
+				} else {
+					expect(date.getDay()).toBe(0);
+				}
+			}
+		}
+	});
+
+	it('should split month to by weeks', () => {
+		// February 2021
+		const year = 2021;
+		const month = 1;
+		const totalDays = 28;
+		const startDate = new Date(year, month);
+		const dates: DatepickerDate[] = [];
+
+		for (let day = 1; day <= totalDays; day++) {
+			dates.push(new DatepickerDate(new Date(year, month, day)));
+		}
+
+		const splittedByWeeks = splitByWeeks(dates);
+
+		expect(splittedByWeeks[0][0].dayOfWeek).toBe(startDate.getDay());
+
+		let dateCounter = 1;
+		for (let i = 0; i < splittedByWeeks.length; i++) {
+			const week = splittedByWeeks[i];
+
+			expect(week.length).toBeLessThanOrEqual(7);
+			expect(week.length).not.toBe(0);
+
+			const weekStart = i === 0 ? week[0].dayOfWeek : 1;
+			for (let j = 0; j < week.length; j++) {
+				const date = week[j];
+
+				expect(date.monthDate).toBe(dateCounter);
+				dateCounter++;
+
+				if (weekStart + j < 7) {
+					expect(date.dayOfWeek).toBe(weekStart + j);
+				} else {
+					expect(date.dayOfWeek).toBe(0);
+				}
+			}
+		}
 	});
 });
