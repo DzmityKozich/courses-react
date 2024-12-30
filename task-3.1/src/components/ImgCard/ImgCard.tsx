c7d4c487971637a474b30a991cde47d3929ce968
Дмитрий Козич <DzmityKozich>
2024-12-30 17:26:21 +0400

    Update ImgCard styles

--- task-3.1/src/components/ImgCard/ImgCard.tsx
+++ task-3.1/src/components/ImgCard/ImgCard.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Img } from '../../models/Img';
 
-import './ImgCard.scss';
-
 type Props = {
 	img: Img;
 };
@@ -10,7 +8,10 @@ type Props = {
 export const ImgCard: React.FC<Props> = ({ img }) => {
 	return (
 		<>
-			<div className="img-card bg-cover bg-center cursor-pointer" style={{ backgroundImage: `url(${img.webformatURL})` }}>
+			<div
+				className="w-[180px] h-[135px] relative bg-cover bg-center cursor-pointer"
+				style={{ backgroundImage: `url(${img.webformatURL})` }}
+			>
 				<p className="absolute left-0 bottom-0 w-full text-center bg-black bg-opacity-60 text-white">{img.user}</p>
 			</div>
 		</>
